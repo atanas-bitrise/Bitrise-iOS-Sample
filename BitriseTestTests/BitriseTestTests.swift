797f5d6e53cf234a813b1b0fbbe2b7ef22096e72
Atanas Chanev <atanas.chanev@bitrise.io>
2021-11-23 14:05:21 +0000

    Added one test

--- BitriseTestTests/BitriseTestTests.swift
+++ BitriseTestTests/BitriseTestTests.swift
@@ -18,10 +18,7 @@ class BitriseTestTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
+    func testExample0() throws { XCTAssertTrue(1==1, "One is not equal to one") }
 
     func testPerformanceExample() throws {
         // This is an example of a performance test case.

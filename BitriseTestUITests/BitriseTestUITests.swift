f164320ce5d0fed65d14c1ab001c276cc6d5cc3e
Atanas Chanev <atanas.chanev@bitrise.io>
2022-12-16 11:56:08 +0000

    Demo

--- BitriseTestUITests/BitriseTestUITests.swift
+++ BitriseTestUITests/BitriseTestUITests.swift
@@ -21,15 +21,15 @@ class BitriseTestUITests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
-
-    func testExample() throws {
+    
+    func testExampleDemo() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
 
         // Use recording to get started writing UI tests.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-        let greetingsLabel = app.staticTexts["Hello world!"]
+        let greetingsLabel = app.staticTexts["Hello, world!"]
         XCTAssertTrue(greetingsLabel.exists)
     }
 
@@ -42,8 +42,8 @@ class BitriseTestUITests: XCTestCase {
         }
     }
     
-//    override func record(_ issue: XCTIssue) {
-//        add(XCTAttachment(screenshot: XCUIScreen.main.screenshot()))
-//        super.record(issue)
-//    }
+    override func record(_ issue: XCTIssue) {
+        add(XCTAttachment(screenshot: XCUIScreen.main.screenshot()))
+        super.record(issue)
+    }
 }
